5808e68969ea9ce7289a8cb24e7ee0407886cf55
zahra987 <zhr.ramezanpour@gmail.com>
2023-05-04 00:28:24 +0430

    add onclick on previous button

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var i=0;
 function handleClick(whichOne) {
     //next
     if (whichOne==='next') {
-      if (i===10) {
+      if (i===10){
           i=0;
         }else{
           i++;
@@ -30,6 +30,22 @@ function handleClick(whichOne) {
      }
     //previous
     if (whichOne==='previous') {
-     
+      if (i===0){
+          i=10;
+        }else{
+          i--;
+        }
+      img1.src=imgAddress[i];
+      img1.style.transform='rotateY(0)';
+      img1.style.transition='all 2s linear';
+      img2.style.transform='rotateY(90deg)';
+      img2.style.transition='all 1s linear';
+      setTimeout(function () {
+      img1.style.transform='rotateY(-180deg)';
+      img1.style.transition='all 0s linear';
+      img2.style.transform='rotateY(0)';
+      img2.style.transition='all 0s linear';
+      img2.src=img1.src;
+      },2000);
     }
 }
